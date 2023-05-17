81c256edd31fa6808ee6c1666f84a5f22a492bcf
Colin Masterson <colinmasterson97@hotmail.com>
2023-05-17 14:39:36 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.phoneticalphabetjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.phoneticalphabetjavafx to javafx.fxml;
+    exports com.example.phoneticalphabetjavafx;
+}
